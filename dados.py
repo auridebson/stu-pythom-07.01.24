9d366ef87e56453eb989cb6bab90f9bf4561099b
Auridebson Lima <auridebson@vutus.com>
2024-01-07 11:44:03 -0300

    Update

--- dados.py
+++ dados.py
@@ -51,3 +51,41 @@ paises_africanos = {
     'Zâmbia': 'Lusaka',
     'Zimbábue': 'Harare'
 }
+
+frutas_tropicais = [
+    'Abacaxi',
+    'Manga',
+    'Goiaba',
+    'Banana',
+    'Coco',
+    'Maracujá',
+    'Caju',
+    'Abacate',
+    'Pitaya',
+    'Graviola',
+    'Kiwi',
+    'Papaya',
+    'Jabuticaba',
+    'Lichia',
+    'Rambutã'
+]
+
+frutas_nutricao = {
+    'Abacaxi': {'Calorias': 50, 'Vitamina C': 47, 'Fibras': 2.3},
+    'Manga': {'Calorias': 60, 'Vitamina A': 54, 'Vitamina C': 60},
+    'Goiaba': {'Calorias': 68, 'Vitamina C': 228, 'Fibras': 8.9},
+    'Banana': {'Calorias': 105, 'Potássio': 422, 'Vitamina B6': 0.4},
+    'Coco': {'Calorias': 354, 'Água': 100, 'Gorduras': 33.5},
+    'Maracujá': {'Calorias': 17, 'Vitamina C': 20, 'Fibras': 5.4},
+    'Caju': {'Calorias': 43, 'Vitamina C': 218, 'Fibras': 0.9},
+    'Abacate': {'Calorias': 160, 'Gorduras': 14.7, 'Fibras': 6.7},
+    'Pitaya': {'Calorias': 60, 'Fibras': 9, 'Vitamina C': 9},
+    'Graviola': {'Calorias': 66, 'Fibras': 3.3, 'Vitamina C': 20},
+    'Kiwi': {'Calorias': 61, 'Vitamina C': 92, 'Fibras': 3},
+    'Papaya': {'Calorias': 59, 'Vitamina C': 62, 'Fibras': 2.5},
+    'Jabuticaba': {'Calorias': 58, 'Fibras': 4.9, 'Vitamina C': 13},
+    'Lichia': {'Calorias': 66, 'Vitamina C': 136, 'Fibras': 2.3},
+    'Rambutã': {'Calorias': 68, 'Vitamina C': 20, 'Fibras': 0.9}
+}
+
+frutas = ["Maça","Limão","Pera",]
--- main.py
+++ main.py
@@ -1,92 +1,43 @@
-paises_africanos = {
-    'Argélia': 'Argel',
-    'Angola': 'Luanda',
-    'Benin': 'Porto Novo',
-    'Botsuana': 'Gaborone',
-    'Burquina Faso': 'Ouagadougou',
-    'Burundi': 'Bujumbura',
-    'Cabo Verde': 'Praia',
-    'Camarões': 'Yaoundé',
-    'Chade': 'Djamena',
-    'Comores': 'Moroni',
-    'Congo': 'Brazzaville',
-    'Costa do Marfim': 'Yamoussoukro',
-    'Djibuti': 'Djibuti',
-    'Egito': 'Cairo',
-    'Eritreia': 'Asmara',
-    'Eswatini': 'Lobamba',
-    'Etiópia': 'Adis Abeba',
-    'Gabão': 'Libreville',
-    'Gâmbia': 'Banjul',
-    'Gana': 'Acra',
-    'Guiné': 'Conacri',
-    'Guiné-Bissau': 'Bissau',
-    'Guiné Equatorial': 'Malabo',
-    'Lesoto': 'Maseru',
-    'Libéria': 'Monróvia',
-    'Líbia': 'Trípoli',
-    'Madagáscar': 'Antananarivo',
-    'Maláui': 'Lilongwe',
-    'Mali': 'Bamako',
-    'Marrocos': 'Rabat',
-    'Maurícia': 'Port Louis',
-    'Mauritânia': 'Nouakchott',
-    'Moçambique': 'Maputo',
-    'Namíbia': 'Windhoek',
-    'Níger': 'Niamey',
-    'Nigéria': 'Abuja',
-    'Quênia': 'Nairóbi',
-    'Ruanda': 'Quigali',
-    'São Tomé e Príncipe': 'São Tomé',
-    'Senegal': 'Dacar',
-    'Seicheles': 'Victoria',
-    'Serra Leoa': 'Freetown',
-    'Somália': 'Mogadíscio',
-    'Sudão': 'Cartum',
-    'Sudão do Sul': 'Juba',
-    'Tanzânia': 'Dodoma',
-    'Togo': 'Lomé',
-    'Tunísia': 'Túnis',
-    'Uganda': 'Kampala',
-    'Zâmbia': 'Lusaka',
-    'Zimbábue': 'Harare'
-}
+from dados import *
 
-frutas_tropicais = [
-    'Abacaxi',
-    'Manga',
-    'Goiaba',
-    'Banana',
-    'Coco',
-    'Maracujá',
-    'Caju',
-    'Abacate',
-    'Pitaya',
-    'Graviola',
-    'Kiwi',
-    'Papaya',
-    'Jabuticaba',
-    'Lichia',
-    'Rambutã'
-]
 
-frutas = ["Maça","Limão","Pera",]
-
+# BLOCO DE FUNÇÕES
 def ln(x):
     print("-"*30)
+
+def add_fruta():
+    fruta = str(input("Digite o nome da fruta: "))
+    frutas_tropicais.append(fruta)
+
+def lista_frutas():
+    for fruta in frutas_tropicais:
+        print(fruta)
+
+    
+
+# FIM BLOCO DE FUNÇÕES
 
 print("Programa para estudar listas")
 ln(45)
 
-# for indice, pais in enumerate(paises_africanos): 
-#     print(f"""País: {pais}\nCapital: {paises_africanos[pais]}""")
-#     ln(30) 
-
 while True:
-    for item, fruta in enumerate(frutas_tropicais):
-        print(f"""{item+1}º - {fruta}""")
-    escolha = int(input("Qual a fruta que você quer deletar? "))
-    if escolha == 100:
-        break
-    frutas_tropicais.pop(escolha-1)
+    escolha = int(input("""Escolha a opção:
+    1 - Adicionar fruta
+    2 - Excluir fruta
+    3 - Listar as frutas
+    0 - Sair
+    """))
 
+    match escolha:
+        case 1:
+            add_fruta()
+        case 2:
+            print("Escolha 2 feita")
+        case 3:
+            lista_frutas()
+        case 0:
+            break
+        case _:
+            print("Opção inválida")
+        
+        
