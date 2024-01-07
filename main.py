e29394b5ab442e93cc7394d0a9654e29e1560048
Auridebson Lima <auridebson@vutus.com>
2024-01-07 10:06:38 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+paises_africanos = {
+    'Argélia': 'Argel',
+    'Angola': 'Luanda',
+    'Benin': 'Porto Novo',
+    'Botsuana': 'Gaborone',
+    'Burquina Faso': 'Ouagadougou',
+    'Burundi': 'Bujumbura',
+    'Cabo Verde': 'Praia',
+    'Camarões': 'Yaoundé',
+    'Chade': 'Djamena',
+    'Comores': 'Moroni',
+    'Congo': 'Brazzaville',
+    'Costa do Marfim': 'Yamoussoukro',
+    'Djibuti': 'Djibuti',
+    'Egito': 'Cairo',
+    'Eritreia': 'Asmara',
+    'Eswatini': 'Lobamba',
+    'Etiópia': 'Adis Abeba',
+    'Gabão': 'Libreville',
+    'Gâmbia': 'Banjul',
+    'Gana': 'Acra',
+    'Guiné': 'Conacri',
+    'Guiné-Bissau': 'Bissau',
+    'Guiné Equatorial': 'Malabo',
+    'Lesoto': 'Maseru',
+    'Libéria': 'Monróvia',
+    'Líbia': 'Trípoli',
+    'Madagáscar': 'Antananarivo',
+    'Maláui': 'Lilongwe',
+    'Mali': 'Bamako',
+    'Marrocos': 'Rabat',
+    'Maurícia': 'Port Louis',
+    'Mauritânia': 'Nouakchott',
+    'Moçambique': 'Maputo',
+    'Namíbia': 'Windhoek',
+    'Níger': 'Niamey',
+    'Nigéria': 'Abuja',
+    'Quênia': 'Nairóbi',
+    'Ruanda': 'Quigali',
+    'São Tomé e Príncipe': 'São Tomé',
+    'Senegal': 'Dacar',
+    'Seicheles': 'Victoria',
+    'Serra Leoa': 'Freetown',
+    'Somália': 'Mogadíscio',
+    'Sudão': 'Cartum',
+    'Sudão do Sul': 'Juba',
+    'Tanzânia': 'Dodoma',
+    'Togo': 'Lomé',
+    'Tunísia': 'Túnis',
+    'Uganda': 'Kampala',
+    'Zâmbia': 'Lusaka',
+    'Zimbábue': 'Harare'
+}
+
+
+def ln(x):
+    print("-"*30)
+
+print("Programa para estudar listas")
+ln(45)
+
+
+for indice, pais in enumerate(paises_africanos):
+    print(f"""País: {pais}\nCapital: {paises_africanos[pais]}""")
+    ln(30)
